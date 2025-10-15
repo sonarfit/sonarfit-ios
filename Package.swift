f35164a80f4e446b7e9df1ee71b1aa2454008bb4
mradamfrancis <ajfrancis@gmail.com>
2025-10-15 23:03:29 +0100

    change zip filename

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SonarFitSDK",
-            url: "https://github.com/sonarfit/sonarfit-ios/releases/download/v1.0.0/SonarFitSDK-1.0.0.xcframework.zip",
+            url: "https://github.com/sonarfit/sonarfit-ios/releases/download/v1.0.0/SonarFitSDK.xcframework.zip",
             checksum: "a27df9b9bd7dabb435056c10d6cf5758ca862fe256c39edb96ede6ffb628c741"
         )
     ]
